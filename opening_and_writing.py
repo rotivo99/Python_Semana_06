ae055fe74e2962974b163967bb4279a4f8f6d108
rotivo <vitorfernando@usp.br>
2022-11-02 13:36:58 -0300

    Código que usei para escrever o arquivo Kröten.txt

---
+++ opening_and_writing.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+with open('Kröten.txt', 'w') as texto:
+    texto.write('Kröten, Frösche, Unken - Was ist der Unterschied?\n')
+    texto.write('In einem Kinderbuch stand der Satz: „Die Kröte hüpfte...“ Prompt kam von Johanna der Einwand: „Kröten hüpfen nicht, die gehen.“ Ach so! Das wissen wahrscheinlich nur Biologen und Laich-Teich-Besitzer und Kröten-Liebhaber oder so. Aber man kann sich ja schlau machen und ich habe mich gefragt, wieso Frösche hüpfen, Kröten aber nicht.\n')
